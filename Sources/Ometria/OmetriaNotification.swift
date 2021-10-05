057ab4679a58e76a19687924590f57907c57650d
Sergiu Corbu <sergiu.corbu@tapptitude.com>
2021-10-05 16:04:10 +0300

    removed OmetriaContext & updated for flat decoding

--- Sources/Ometria/OmetriaNotification.swift
+++ Sources/Ometria/OmetriaNotification.swift
@@ -23,8 +23,10 @@ public struct OmetriaNotification: Decodable {
     
     public var deepLink: String?
     public var imageUrl: String?
-    public var externalCustomerId: String
-    public var context: OmetriaNotificationContext
+    public var externalCustomerId: String?
+    public var campaignType: String?
+    public var sendId: String?
+    public var tracking: [String:Any]?
     
     enum CodingKeys: String, CodingKey {
         case externalCustomerId = "ext_customer_id"
@@ -32,33 +34,32 @@ public struct OmetriaNotification: Decodable {
         case imageUrl
         case context
     }
-}
-
-
-public struct OmetriaNotificationContext: Decodable {
     
-    public var campaignType: String?
-    public var sendId: String?
-    public var tracking: [String:Any]?
+    enum TrackingKeys: CodingKey {
+        case tracking
+    }
     
-    enum CodingKeys: String, CodingKey {
-        
+    enum ContextKeys: String, CodingKey {
         case campaignType = "campaign_type"
         case sendId = "send_id"
         case tracking
     }
     
     public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        campaignType = try? values.decode(String.self, forKey: .campaignType)
-        sendId = try? values.decode(String.self, forKey: .sendId)
-
-        guard values.contains(.tracking),
-              let jsonData = try? values.decode(Data.self, forKey: .tracking) else {
+        let valuesContainer = try decoder.container(keyedBy: CodingKeys.self)
+        deepLink = try? valuesContainer.decode(String.self, forKey: .deepLink)
+        imageUrl = try? valuesContainer.decode(String.self, forKey: .imageUrl)
+        externalCustomerId = try? valuesContainer.decode(String.self, forKey: .externalCustomerId)
+        
+        let contextContainer = try valuesContainer.nestedContainer(keyedBy: ContextKeys.self, forKey: .context)
+        campaignType = try? contextContainer.decode(String.self, forKey: .campaignType)
+        sendId = try? contextContainer.decode(String.self, forKey: .sendId)
+        
+        guard contextContainer.contains(.tracking),
+              let jsonData = try? contextContainer.decode(Data.self, forKey: .tracking) else {
                   tracking = [:]
                   return
         }
         tracking = (try? JSONSerialization.jsonObject(with: jsonData) as? [String : Any]) ?? [String:Any]()
-
     }
 }

616dfcae3329686f0eb9aa82dcd485eb2ff1f0fa
Dumitru Igor <>
2026-01-29 09:44:24 +0200

    Increased version to 1.8.5

--- Sources/Ometria/Constants.swift
+++ Sources/Ometria/Constants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public enum Constants {
-    static let sdkVersion = "1.8.4"
+    static let sdkVersion = "1.8.5"
     public static let defaultTriggerLimit = 10
     public static let defaultFlushInterval = 60
     static let flushMaxBatchSize = 100

7fb5940ba0452d07d9292d697cf25f5c40035b87
Dumitru Igor <>
2026-02-13 15:58:21 +0200

    Fixed swift tool version for SPM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ometria.ios_sdk",
+    name: "Ometria",
     platforms: [.iOS(.v15)],
     products: [
         .library(
